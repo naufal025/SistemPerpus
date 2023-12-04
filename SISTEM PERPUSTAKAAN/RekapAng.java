ecab0d51ffce70f73d96a4cf0f1a488e52432282
hifnazwa <144086644+hifnazwa@users.noreply.github.com>
2023-12-04 08:46:30 +0700

    Update RekapAng.java
    pengubahan metode

--- SISTEM PERPUSTAKAAN/RekapAng.java
+++ SISTEM PERPUSTAKAAN/RekapAng.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
-public class RekapAng {
-    public static void main(String[] args) {
-        Scanner sc22 = new Scanner(System.in);
 
-        String nama;
-        char jawab;
-        int jml = 0;
-        do{
-            System.out.print("Masukkan nama : ");
-            nama = sc22.nextLine();
-            jml++;
-            System.out.print("Apakah anda ingin memasukkan nama anggota baru (Y/T)? ");
-            jawab = sc22.nextLine().charAt(0);
-        } while (jawab == 'Y' || jawab == 'y');
-            System.out.println("Jumlah anggota sekarang = " + jml);
-    }
+public class RekapAng {
+
+    static String[][] anggota = new String[100][2]; 
+    static int index = 0;
     
+    public static void main(String[] args) {
+        inputData();
+        tampilData();
+    }
+
+    static void inputData() {
+        Scanner input = new Scanner(System.in);
+        char lagi = 'y';
+        
+        while(lagi == 'Y' || lagi == 'y') {
+            System.out.print("Masukkan Nama: ");
+            String nama = input.nextLine();
+            
+            System.out.print("Masukkan Umur: ");
+            String umur = input.nextLine();
+            
+            anggota[index][0] = nama;
+            anggota[index][1] = umur; 
+            index++;
+            
+            System.out.print("Tambah lagi (Y/T)?");
+            lagi = input.nextLine().charAt(0);
+        }
+    }
+
+    static void tampilData() {
+        System.out.println("Data Anggota:");
+        for(int i = 0; i < index; i++) {
+            System.out.println("Nama: " + anggota[i][0]); 
+            System.out.println("Umur: " + anggota[i][1]);}
+    }
 }
