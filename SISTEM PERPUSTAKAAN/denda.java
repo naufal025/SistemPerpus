25e70c32e8ef51ff7ea450e1ad70c8bbc7a46c93
hifnazwa <144086644+hifnazwa@users.noreply.github.com>
2023-12-04 08:47:30 +0700

    Update denda.java
    pengubahan metode

--- SISTEM PERPUSTAKAAN/denda.java
+++ SISTEM PERPUSTAKAAN/denda.java
@@ -1,29 +1,68 @@
 import java.util.Scanner;
 
 public class denda {
+    
+    static String[] namaPeminjam = new String[100];
+static int[][] dataAnggota = new int[100][2];
+    static int index = 0;
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-
-        int hariTerlambat,jumlahPoin;
-        int poin = 0;
-
-        System.out.print("Berapa hari anda terlambat mengembalikan buku : ");
-        hariTerlambat = sc.nextInt();
-        if (hariTerlambat <= 5){
-            jumlahPoin = hariTerlambat + poin;
-            System.out.println("Total poin anda : " + jumlahPoin);
-            if (jumlahPoin >= 3){
-            System.out.println("Mendapatkan hukuman resume buku");
+        Scanner input = new Scanner(System.in);
+        
+        int menu;
+        
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Input Data Peminjam"); 
+            System.out.println("2. Tampilkan Data Peminjam");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+            menu = input.nextInt();
+            
+            if(menu == 1) {
+                inputData();
+            } else if(menu == 2) {
+                tampilData();
+            } else {
+                System.out.println("Terimakasih sudah menggunakan sistem denda");
             }
-        } else if (hariTerlambat > 5) {
-            jumlahPoin = hariTerlambat + poin;
-            System.out.println("Total poin anda : " + jumlahPoin);
-            if (jumlahPoin >= 10){
-            System.out.println("Mendapatkan hukuman membersihkan perpustakaan");
-            }
-        } else {
-            System.out.println("Anda tidak mendapatkan poin apa apa");
-        }       
+        } while(menu != 3);
+    }
+    
+    static void inputData() {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Nama peminjam: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Berapa hari terlambat: ");  
+        int hari = input.nextInt();
+        
+        int poin = hitungPoin(hari);
+        
+        namaPeminjam[index] = nama; 
+        dataAnggota[index][0] = hari;
+        dataAnggota[index][1] = poin;
+        
+        index++;
+    }
+    
+    static void tampilData() {
+        for(int i = 0; i < index; i++) {
+            System.out.println("Nama: " + namaPeminjam[i]);
+            System.out.println("Hari terlambat: " + dataAnggota[i][0]); 
+            System.out.println("Poin: " + dataAnggota[i][1]);
+            System.out.println();  
         }
     }
-
+    
+    static int hitungPoin(int hari) {
+        int poin = 0;
+        if(hari <= 5) {
+            poin = hari; 
+        } else if(hari > 5){
+            poin = 10;
+        }
+        return poin;
+    }
+}
